02fb42f4799cae8e39232bddab77e96aa0fb99c3
ismayilkerimzade <ismayil.programmer@gmail.com>
2023-04-22 18:24:56 +0400

    first

---
+++ MailSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MailSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
